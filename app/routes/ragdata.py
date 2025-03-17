62359a3774a56804dfa98ea7c26c9f1d7a4dbb9f
gunapark <ncc0816@gmail.com>
2025-03-17 23:24:40 +0900

    소스코드 통합

--- app/routes/ragdata.py
+++ app/routes/ragdata.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.service.rag.ragdataservice import getdata
 from app.service.rag.chucnking import chunk_text_by_sentence
+import os
+import numpy as np
 
 router = APIRouter()
 
@@ -9,4 +11,6 @@ async def rag_data_route(url: str):
   text = getdata(url)
   chunks = chunk_text_by_sentence(text)
   #임베딩 + 원본 텍스트 따로 저장
+  #임베딩 모델: upskyy/bge-m3-korean
+  #원본 텍스트: Elastic search를 사용.
   return {"message": "!!"}
